19b574d68b1a040eca8cae0d334cee25693013f8
Lei Tang <32078630+lei-tang@users.noreply.github.com>
2018-04-13 15:11:16 -0700

    Add github.com/hashicorp/vault to vendor dependency

---
+++ code.cloudfoundry.org/copilot/handlers/models_test.go
@@ -0,0 +1,262 @@
+package handlers_test
+
+import (
+	"code.cloudfoundry.org/copilot/handlers"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Handler Models", func() {
+	Describe("RoutesRepo", func() {
+		var routesRepo handlers.RoutesRepo
+
+		BeforeEach(func() {
+			routesRepo = handlers.RoutesRepo{
+				Repo: make(map[handlers.RouteGUID]*handlers.Route),
+			}
+		})
+
+		It("can Upsert and Delete routes", func() {
+			route := &handlers.Route{
+				Host: "host.example.com",
+				GUID: "some-route-guid",
+			}
+
+			go routesRepo.Upsert(route)
+
+			Eventually(func() *handlers.Route {
+				r, _ := routesRepo.Get("some-route-guid")
+				return r
+			}).Should(Equal(route))
+
+			routesRepo.Delete(route.GUID)
+
+			r, ok := routesRepo.Get("some-route-guid")
+			Expect(ok).To(BeFalse())
+			Expect(r).To(BeNil())
+		})
+
+		It("does not error when deleting a route that does not exist", func() {
+			route := handlers.Route{
+				Host: "host.example.com",
+				GUID: "delete-me",
+			}
+
+			routesRepo.Delete(route.GUID)
+			routesRepo.Delete(route.GUID)
+
+			_, ok := routesRepo.Get("delete-me")
+			Expect(ok).To(BeFalse())
+		})
+
+		It("can Upsert the same route twice", func() {
+			route := &handlers.Route{
+				Host: "host.example.com",
+				GUID: "some-route-guid",
+			}
+
+			updatedRoute := &handlers.Route{
+				Host: "something.different.com",
+				GUID: route.GUID,
+			}
+
+			routesRepo.Upsert(updatedRoute)
+
+			r, _ := routesRepo.Get("some-route-guid")
+			Expect(r).To(Equal(updatedRoute))
+		})
+
+		It("can Sync routes", func() {
+			route := &handlers.Route{
+				Host: "host.example.com",
+				GUID: "some-route-guid",
+			}
+
+			go routesRepo.Upsert(route)
+
+			Eventually(func() *handlers.Route {
+				r, _ := routesRepo.Get("some-route-guid")
+				return r
+			}).Should(Equal(route))
+
+			newRoute := &handlers.Route{
+				Host: "host.example.com",
+				GUID: "some-other-route-guid",
+			}
+
+			routesRepo.Sync([]*handlers.Route{newRoute})
+			Expect(routesRepo.List()).To(Equal(map[string]string{
+				string(newRoute.GUID): newRoute.Host,
+			}))
+		})
+	})
+
+	Describe("RouteMappingsRepo", func() {
+		var routeMappingsRepo *handlers.RouteMappingsRepo
+		BeforeEach(func() {
+			routeMappingsRepo = &handlers.RouteMappingsRepo{
+				Repo: make(map[string]*handlers.RouteMapping),
+			}
+		})
+
+		It("can Map and Unmap Routes", func() {
+			routeMapping := handlers.RouteMapping{
+				RouteGUID:       "some-route-guid",
+				CAPIProcessGUID: "some-capi-guid",
+			}
+
+			go routeMappingsRepo.Map(&routeMapping)
+
+			Eventually(routeMappingsRepo.List).Should(Equal(map[string]*handlers.RouteMapping{
+				routeMapping.Key(): &routeMapping,
+			}))
+
+			routeMappingsRepo.Unmap(&routeMapping)
+			Expect(routeMappingsRepo.List()).To(HaveLen(0))
+		})
+
+		It("does not duplicate route mappings", func() {
+			routeMapping := handlers.RouteMapping{
+				RouteGUID:       "some-route-guid",
+				CAPIProcessGUID: "some-capi-guid",
+			}
+
+			routeMappingsRepo.Map(&routeMapping)
+			routeMappingsRepo.Map(&routeMapping)
+			routeMappingsRepo.Map(&routeMapping)
+
+			Expect(routeMappingsRepo.List()).To(HaveLen(1))
+		})
+
+		It("can Sync RouteMappings", func() {
+			routeMapping := handlers.RouteMapping{
+				RouteGUID:       "some-route-guid",
+				CAPIProcessGUID: "some-capi-guid",
+			}
+
+			routeMappingsRepo.Map(&routeMapping)
+
+			newRouteMapping := handlers.RouteMapping{
+				RouteGUID:       "some-other-route-guid",
+				CAPIProcessGUID: "some-other-capi-guid",
+			}
+			updatedRouteMappings := []*handlers.RouteMapping{&newRouteMapping}
+
+			routeMappingsRepo.Sync(updatedRouteMappings)
+
+			Eventually(routeMappingsRepo.List).Should(Equal(map[string]*handlers.RouteMapping{
+				newRouteMapping.Key(): &newRouteMapping,
+			}))
+		})
+	})
+
+	Describe("CAPIDiegoProcessAssociationsRepo", func() {
+		var capiDiegoProcessAssociationsRepo handlers.CAPIDiegoProcessAssociationsRepo
+		BeforeEach(func() {
+			capiDiegoProcessAssociationsRepo = handlers.CAPIDiegoProcessAssociationsRepo{
+				Repo: make(map[handlers.CAPIProcessGUID]*handlers.CAPIDiegoProcessAssociation),
+			}
+		})
+
+		It("can upsert and delete CAPIDiegoProcessAssociations", func() {
+			capiDiegoProcessAssociation := handlers.CAPIDiegoProcessAssociation{
+				CAPIProcessGUID: "some-capi-process-guid",
+				DiegoProcessGUIDs: handlers.DiegoProcessGUIDs{
+					"some-diego-process-guid-1",
+					"some-diego-process-guid-2",
+				},
+			}
+
+			go capiDiegoProcessAssociationsRepo.Upsert(&capiDiegoProcessAssociation)
+
+			capiProcessGUID := handlers.CAPIProcessGUID("some-capi-process-guid")
+
+			Eventually(func() *handlers.CAPIDiegoProcessAssociation {
+				return capiDiegoProcessAssociationsRepo.Get(&capiProcessGUID)
+			}).Should(Equal(&capiDiegoProcessAssociation))
+
+			capiDiegoProcessAssociationsRepo.Delete(&capiDiegoProcessAssociation.CAPIProcessGUID)
+			Expect(capiDiegoProcessAssociationsRepo.Get(&capiProcessGUID)).To(BeNil())
+		})
+
+		It("can sync CAPIDiegoProcessAssociations", func() {
+			capiDiegoProcessAssociation := &handlers.CAPIDiegoProcessAssociation{
+				CAPIProcessGUID: "some-capi-process-guid",
+				DiegoProcessGUIDs: handlers.DiegoProcessGUIDs{
+					"some-diego-process-guid-1",
+					"some-diego-process-guid-2",
+				},
+			}
+
+			capiDiegoProcessAssociationsRepo.Upsert(capiDiegoProcessAssociation)
+
+			newCapiDiegoProcessAssociation := &handlers.CAPIDiegoProcessAssociation{
+				CAPIProcessGUID: "some-other-capi-process-guid",
+				DiegoProcessGUIDs: handlers.DiegoProcessGUIDs{
+					"some-diego-process-guid-1",
+					"some-diego-process-guid-2",
+				},
+			}
+
+			capiDiegoProcessAssociationsRepo.Sync([]*handlers.CAPIDiegoProcessAssociation{newCapiDiegoProcessAssociation})
+
+			Expect(capiDiegoProcessAssociationsRepo.List()).To(Equal(map[handlers.CAPIProcessGUID]*handlers.DiegoProcessGUIDs{
+				"some-other-capi-process-guid": &newCapiDiegoProcessAssociation.DiegoProcessGUIDs,
+			}))
+		})
+	})
+
+	Describe("RouteMapping", func() {
+		Describe("Key", func() {
+			It("is unique for process guid and route guid", func() {
+				rmA := handlers.RouteMapping{
+					RouteGUID:       "route-guid-1",
+					CAPIProcessGUID: "some-capi-guid-1",
+				}
+
+				rmB := handlers.RouteMapping{
+					RouteGUID:       "route-guid-1",
+					CAPIProcessGUID: "some-capi-guid-2",
+				}
+
+				rmC := handlers.RouteMapping{
+					RouteGUID:       "route-guid-2",
+					CAPIProcessGUID: "some-capi-guid-1",
+				}
+
+				Expect(rmA.Key()).NotTo(Equal(rmB.Key()))
+				Expect(rmA.Key()).NotTo(Equal(rmC.Key()))
+				Expect(rmB.Key()).NotTo(Equal(rmC.Key()))
+			})
+		})
+	})
+
+	Describe("DiegoProcessGUIDs", func() {
+		Describe("ToStringSlice", func() {
+			It("returns the guids as a slice of strings", func() {
+				diegoProcessGUIDs := handlers.DiegoProcessGUIDs{
+					"some-diego-process-guid-1",
+					"some-diego-process-guid-2",
+				}
+				Expect(diegoProcessGUIDs.ToStringSlice()).To(Equal([]string{
+					"some-diego-process-guid-1",
+					"some-diego-process-guid-2",
+				}))
+			})
+		})
+	})
+
+	Describe("DiegoProcessGUIDsFromStringSlice", func() {
+		It("returns the guids as DiegoProcessGUIDs", func() {
+			diegoProcessGUIDs := []string{
+				"some-diego-process-guid-1",
+				"some-diego-process-guid-2",
+			}
+			Expect(handlers.DiegoProcessGUIDsFromStringSlice(diegoProcessGUIDs)).To(Equal(handlers.DiegoProcessGUIDs{
+				"some-diego-process-guid-1",
+				"some-diego-process-guid-2",
+			}))
+		})
+	})
+})
---
+++ code.cloudfoundry.org/copilot/sdk/ruby/lib/copilot/version.rb
@@ -0,0 +1,5 @@
+module Cloudfoundry
+  module Copilot
+    VERSION = "0.0.3"
+  end
+end
